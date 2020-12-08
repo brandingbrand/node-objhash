3e55043c5b80cd572e30ed64801e88d39259807f
Cauldrath <ben@brandingbrand.com>
2020-12-08 17:58:23 -0500

    chore: Update to support node 12+
    Node 12 updated to a version of v8 where v8::Value::ToObject requires a v8::Context and returns a MaybeLocal<v8::Object> instead of an object:
    https://v8docs.nodesource.com/node-12.19/dc/d0a/classv8_1_1_value.html#ae098e684dad8f790e7529ccce5bfd37e
    
    This updates dependencies (keeping mocha at 7 because 8 wasn't working with Node 8) and fixes the above issue.

--- src/objhash.cc
+++ src/objhash.cc
@@ -1,21 +1,26 @@
 #include <nan.h>
+#include <node.h>
+#include <v8.h>
 
 void Method(const Nan::FunctionCallbackInfo<v8::Value>& info) {
   if (info.Length() < 1) {
     info.GetReturnValue().Set(Nan::Undefined());
   } else if (info[0]->IsObject()) {
-    int hash = info[0]->ToObject()->GetIdentityHash();
-    info.GetReturnValue().Set(Nan::New(hash));
+    v8::Local<v8::Object> object;
+    if (info[0]->ToObject(info.GetIsolate()->GetCurrentContext()).ToLocal(&object)) {
+      int hash = object->GetIdentityHash();
+      info.GetReturnValue().Set(Nan::New(hash));
+    } else {
+      info.GetReturnValue().Set(Nan::Undefined());
+    }
   } else {
     info.GetReturnValue().Set(info[0]);
   }
 }
 
-void Init(v8::Handle<v8::Object> exports) {
-  exports->Set(
-    Nan::New("ident").ToLocalChecked(),
-    Nan::New<v8::FunctionTemplate>(Method)->GetFunction()
-  );
+NAN_MODULE_INIT(Init) {
+  Nan::Set(target, Nan::New<v8::String>("objhash").ToLocalChecked(),
+    Nan::GetFunction(Nan::New<v8::FunctionTemplate>(Method)).ToLocalChecked());
 }
 
-NODE_MODULE(objhash, Init)
+NODE_MODULE(NODE_GYP_MODULE_NAME, Init)
